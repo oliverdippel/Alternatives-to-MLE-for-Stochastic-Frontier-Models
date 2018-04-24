78009d4865c644e94c0bed189d2c7d1601c4e724
Oliver Dippel <oliverdippel@olivers-air.speedportentry2090126250021>
2018-04-24 18:52:10 +0200

    more lines

--- basic.R
+++ basic.R
@@ -1,1 +1,3 @@
-#Alternatives to MLE for Stochastic Frontier Models for Panel data
+# Alternatives to MLE for Stochastic Frontier Models for Panel data
+# by Clemens Haerder & Oliver Dippel
+# based on Wang and Ho [2010]
